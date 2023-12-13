77141acfb14fef2e91a2ff2560229b13deeda6d9
jiangliu <1135681182@.com>
2023-12-13 17:19:00 +0800

    css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <button @click="fn">dianji</button>
+    <button @click="fn">点击</button>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
